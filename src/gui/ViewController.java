c1565d7feeb28d5cd8d9d26aad0503bd7340cd51
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-02-03 00:35:45 -0300

    Usando TextField

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,18 +1,40 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 	
 	@FXML
-	private Button btTest;
+	private TextField txtNumber1;
+
+	@FXML
+	private TextField txtNumber2;
 	
 	@FXML
-	public void onBtTestAction() {
-		Alerts.showAlert("Alert title", null, "Hello", AlertType.INFORMATION);
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	public void onBtSumAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double sum = number1 + number2;
+			labelResult.setText(String.format("%.2f", sum));
+		}
+		catch(NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
